f58b2f629c83da1af0e790a97704f479df48cb63
Filip Hracek <filip.hracek@gmail.com>
2016-07-13 16:08:14 -0700

    Tweak output of check_sitemap.rb

--- deploy/check_url.sh
+++ deploy/check_url.sh
@@ -2,9 +2,11 @@
 
 response=$(curl -s -L -D - $1 -o /dev/null);
 if [[ $response =~ "HTTP/1.1 404 Not Found" ]]; then
+  echo ""
   echo "NOT FOUND: $1"
+  echo ""
   exit 1
 else
-  echo "OK: $1"
+  echo -n "."
   exit 0
 fi
