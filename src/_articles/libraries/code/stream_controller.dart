100431384dbe72c5393e056b531fca3d63a3ffc3
Shams Zakhour <shaza@google.com>
2016-09-06 13:27:35 -0700

    Adding a page on static analysis options. (#123)
    * Adding a page on static analysis options.
    
    * Incorporating Brian's feedback.
    
    * Fixing the firebase location.
    
    * Updating file name.
    
    * Last bit of tweaking before final review.
    
    * Tweaks
    
    * Updating as per feedback.
    
    * Further tweaks.
    
    * Correcting info on the strong mode flags.
    
    * Feedback on the strong mode section.

--- src/_articles/libraries/code/stream_controller.dart
+++ src/_articles/libraries/code/stream_controller.dart
@@ -10,7 +10,7 @@ Stream<int> timedCounter(Duration interval, [int maxCount]) {
     controller.add(counter); // Ask stream to send counter values as event.
     if (maxCount != null && counter >= maxCount) {
       timer.cancel();
-      controller.close();    // Ask stream to shut down and tell listeners.
+      controller.close(); // Ask stream to shut down and tell listeners.
     }
   }
 
@@ -45,14 +45,14 @@ main() {
 
 showBasicUsage() {
   Stream<int> counterStream = timedCounter(const Duration(seconds: 1), 15);
-  counterStream.listen(print);      // Print an integer every second, 15 times.
+  counterStream.listen(print); // Print an integer every second, 15 times.
 }
 
 demoPause() {
   Stream<int> counterStream = timedCounter(const Duration(seconds: 1), 15);
   StreamSubscription<int> subscription;
   subscription = counterStream.listen((int counter) {
-    print(counter);  // Print an integer every second.
+    print(counter); // Print an integer every second.
     if (counter == 5) {
       // After 5 ticks, pause for five seconds, then resume.
       subscription.pause();
@@ -62,29 +62,25 @@ demoPause() {
 }
 
 void useMap() {
-  Stream<int> counterStream2 =
-      timedCounter(const Duration(seconds: 1), 15)
-      .map((int x) => x * 2);       // Double the integer in each event.
+  Stream<int> counterStream2 = timedCounter(const Duration(seconds: 1), 15)
+      .map((int x) => x * 2); // Double the integer in each event.
   counterStream2.listen(print);
 }
 
 void useWhere() {
-  Stream<int> counterStream2 =
-      timedCounter(const Duration(seconds: 1), 15)
-      .where((int x) => x.isEven);      // Retain only even integer events.
+  Stream<int> counterStream2 = timedCounter(const Duration(seconds: 1), 15)
+      .where((int x) => x.isEven); // Retain only even integer events.
   counterStream2.listen(print);
 }
 
 void useExpand() {
-  Stream<int> counterStream2 =
-      timedCounter(const Duration(seconds: 1), 15)
-      .expand((var x) => [x, x]);       // Duplicate each event.
+  Stream<int> counterStream2 = timedCounter(const Duration(seconds: 1), 15)
+      .expand((var x) => [x, x]); // Duplicate each event.
   counterStream2.listen(print);
 }
 
 void useTake() {
-  Stream<int> counterStream2 =
-      timedCounter(const Duration(seconds: 1), 15)
-      .take(5);                         // Stop after the first five events.
+  Stream<int> counterStream2 = timedCounter(const Duration(seconds: 1), 15)
+      .take(5); // Stop after the first five events.
   counterStream2.listen(print);
 }
