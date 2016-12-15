c140e1a1de923fd2837cf5c615df7deb9b7336be
Kathy Walrath <kathyw@google.com>
2016-12-15 12:23:52 -0800

    Specify the default deploy project

--- deploy/deploy-firebase.sh
+++ deploy/deploy-firebase.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 echo "================ Deploy to Firebase ========================"
-firebase deploy --token "${FIREBASE_TOKEN}" --non-interactive
+firebase deploy --token "${FIREBASE_TOKEN}" --non-interactive --project default
 
