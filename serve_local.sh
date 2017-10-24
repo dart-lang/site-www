8a015ada4cae79a07504e935c682ae55112120b2
Filip Hracek <filiph@users.noreply.github.com>
2017-10-24 14:38:41 -0700

    Move from firebase to superstatic for serve (#405)
    * Remove 2016 videos banner from front page
    * Remove linkcheck from repo
    Use the package instead.
    
    * Move from firebase to superstatic for serve
    
    For serve_local.sh, `firebase` is an overkill, and it needs logged-in user. `superstatic` is Firebase’s CLI for serving that doesn’t require login.
    
    This is more convenient when you have more than one Firebase accounts and would need to switch to the correct one anytime you want to test the site.
    
    More importantly, it unlocks he possibility to test the site (`linkcheck`) on Travis, as is the case in the site-webdev repo.

--- serve_local.sh
+++ serve_local.sh
@@ -8,14 +8,10 @@ if [[ "$1" == "--dev" && -e _config_dev.yml ]]; then
   CONFIG="--config _config.yml,_config_dev.yml"
 fi
 
-if [[ "$1" == "frontpage" ]]; then
-  CONFIG="--config _config.yml,scripts/frontpage_only_config.yml"
-fi
-
 bundle exec jekyll build $CONFIG --incremental --watch &
 j_pid=$!
-firebase serve --port 4000 &
+superstatic --port 4000 &
 f_pid=$!
-echo "catched PIDs: $j_pid, $f_pid"
+echo "cached PIDs: $j_pid, $f_pid"
 trap "{ kill $j_pid; kill $f_pid; exit 0;}" SIGINT
 wait
