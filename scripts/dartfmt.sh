c7b0f376917e06e22e99781806f95f2947d7fc56
Patrice Chalin <chalin@users.noreply.github.com>
2017-11-12 21:38:34 -0500

    Rename example subfolders (#426)
    - `language-tour` --> `language_tour`
    - ` library-tour` --> ` library_tour`

--- scripts/dartfmt.sh
+++ scripts/dartfmt.sh
@@ -12,17 +12,17 @@ $DARTFMT -w $EXAMPLES
 
 # Re-run dartfmt with custom settings for the few files that need it:
 $DARTFMT -l 60 \
-  examples/language-tour/color/bin/main.dart \
-  examples/language-tour/factory-constructor/bin/main.dart \
-  examples/language-tour/mixins/bin/main.dart \
-  examples/language-tour/no-such-method/bin/main.dart \
-  examples/language-tour/optional-positional-parameter-default/bin/main.dart \
-  examples/language-tour/reference/immutable_point.dart \
-  examples/language-tour/string-interpolation/bin/main.dart
+  examples/language_tour/color/bin/main.dart \
+  examples/language_tour/factory-constructor/bin/main.dart \
+  examples/language_tour/mixins/bin/main.dart \
+  examples/language_tour/no-such-method/bin/main.dart \
+  examples/language_tour/optional-positional-parameter-default/bin/main.dart \
+  examples/language_tour/reference/immutable_point.dart \
+  examples/language_tour/string-interpolation/bin/main.dart
 
-$DARTFMT -l 62 examples/language-tour/flow/exceptions/web/main.dart
+$DARTFMT -l 62 examples/language_tour/flow/exceptions/web/main.dart
 
-$DARTFMT -l 75 examples/language-tour/flow/break-continue/web/main.dart
+$DARTFMT -l 75 examples/language_tour/flow/break-continue/web/main.dart
 
 # If any files were changed, then exit 1:
 REFORMATTED_FILES=$(git status --short)
