2cc346d6e3f2b46c9a3380e3cd41eb4af5c7ef46
Patrice Chalin <pchalin@gmail.com>
2017-12-05 16:06:36 -0500

    Adjust scripts to use examples/tours path

--- scripts/dartfmt.sh
+++ scripts/dartfmt.sh
@@ -7,29 +7,29 @@ set -e -o pipefail
 cd `dirname $0`/..
 
 DARTFMT="dartfmt -w"
-EXAMPLES=examples
+EXAMPLES=examples/tours
 
 # Format all example source files under lib and tests
 # except for the lib/pi_*.* files
-$DARTFMT -w $EXAMPLES/lib/[^p] $EXAMPLES/test
+$DARTFMT -w $EXAMPLES/lib/{*_tour,util} $EXAMPLES/test
 
 $DARTFMT -l 60 \
-  examples/lib/language_tour/classes/immutable_point.dart \
-  examples/lib/language_tour/classes/logger.dart \
-  examples/lib/language_tour/classes/no_such_method.dart \
-  examples/lib/language_tour/classes/orchestra.dart \
-  examples/test/language_tour/built_in_types_test.dart \
-  examples/test/language_tour/functions_test.dart \
-  examples/lib/library_tour/core \
-  examples/test/library_tour/html_test.dart
+  $EXAMPLES/lib/language_tour/classes/immutable_point.dart \
+  $EXAMPLES/lib/language_tour/classes/logger.dart \
+  $EXAMPLES/lib/language_tour/classes/no_such_method.dart \
+  $EXAMPLES/lib/language_tour/classes/orchestra.dart \
+  $EXAMPLES/test/language_tour/built_in_types_test.dart \
+  $EXAMPLES/test/language_tour/functions_test.dart \
+  $EXAMPLES/lib/library_tour/core \
+  $EXAMPLES/test/library_tour/html_test.dart
 
 $DARTFMT -l 65 \
-  examples/lib/language_tour/exceptions.dart \
-  examples/test/library_tour/core_test.dart \
-  examples/lib/library_tour/async/future.dart \
-  examples/lib/library_tour/async/stream.dart \
-  examples/test/library_tour/io_test.dart \
-  examples/test/library_tour/mirrors_test.dart
+  $EXAMPLES/lib/language_tour/exceptions.dart \
+  $EXAMPLES/test/library_tour/core_test.dart \
+  $EXAMPLES/lib/library_tour/async/future.dart \
+  $EXAMPLES/lib/library_tour/async/stream.dart \
+  $EXAMPLES/test/library_tour/io_test.dart \
+  $EXAMPLES/test/library_tour/mirrors_test.dart
 
 # If any files were changed, then exit 1:
 REFORMATTED_FILES=$(git status --short)
