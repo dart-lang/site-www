9b58a893d3e2cb310c0c90e2e88e1df1b43c67f2
Patrice Chalin <chalin@users.noreply.github.com>
2018-03-15 12:58:37 -0400

    chore: use code-excerpter npm package (1.x) (#675)

--- scripts/refresh-code-excerpts.sh
+++ scripts/refresh-code-excerpts.sh
@@ -4,20 +4,6 @@ set -e -o pipefail
 
 readonly rootDir="$(cd "$(dirname "$0")/.." && pwd)"
 
-function gulpCreateFrag() {
-  pushd $rootDir > /dev/null
-  # Until site-www has its own shredder, use the one from site-webdev
-  if [[ -d $WEBDEV_REPO ]]; then
-    cd $WEBDEV_REPO;
-    gulp create-www-fragments;
-  else
-    echo "ERROR: you cannot run this script without webdev repo at "$WEBDEV_REPO".";
-    echo "See the README for setup instructions.";
-    exit 1;
-  fi
-  popd > /dev/null
-}
-
 function usage() {
   echo $1; echo
   echo "Usage: $(basename $0) [path-to-src-file-or-folder]"; echo
@@ -28,7 +14,17 @@ if [[ $1 == '-h' || $1 == '--help' ]]; then usage; fi
 
 [[ -z "$NGIO_ENV_DEFS" ]] && . $rootDir/scripts/env-set.sh
 
-gulpCreateFrag;
+SRC="$1"
+: ${SRC:="$rootDir/src"}
+[[ -e $SRC ]] || usage "ERROR: source file/folder does not exist: '$SRC'"
+
+FRAG="$rootDir/tmp/_fragments"
+
+if [[ -e $FRAG ]]; then rm -Rf "$FRAG"; echo removing old frag; fi
+
+$(npm bin)/code-excerpter examples "$FRAG"
+
+[[ -e $FRAG ]] || usage "ERROR: fragments folder does not exist: '$FRAG'"
 
 ARGS='--no-escape-ng-interpolation '
 ARGS+='--replace='
@@ -40,13 +36,6 @@ ARGS+='/\{\/\*-(\s*\.\.\.\s*)-\*\/\}/$1/g;' # {/*-...-*/} --> ... (removed brack
 # Replace "//!analysis-issue" by, say, "Analysis issue" (although once we can use embedded DPs this won't be needed0)?
 ARGS+='/\/\/!(analysis-issue|runtime-error)[^\n]*//g;' # Removed warning/error marker
 ARGS+='/\x20*\/\/\s+ignore_for_file:[^\n]+\n//g;' # Remove warning/error marker
-
-SRC="$1"
-: ${SRC:="$rootDir/src"}
-[[ -e $SRC ]] || usage "ERROR: source file/folder does not exist: '$SRC'"
-
-FRAG="$rootDir/tmp/_fragments$API"
-[[ -e $FRAG ]] || usage "ERROR: fragments folder does not exist: '$FRAG'"
 
 echo "Source:     $SRC"
 echo "Fragments:  $FRAG"
