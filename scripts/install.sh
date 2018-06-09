fc88cdf886383564d02f9b503cd17f31df084163
Patrice Chalin <pchalin@gmail.com>
2018-06-09 12:56:03 -0400

    Drop old code-excerpter npm package

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,10 +13,6 @@ else
     echo "Node version: $(node --version)"
 fi
 
-travis_fold start install.npm_install
-(set -x; npm install --no-optional)
-travis_fold end install.npm_install
-
 travis_fold start install.bundle
 (set -x; bundle install)
 travis_fold end install.bundle
