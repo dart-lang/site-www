a39ea88a3ea2873ef62e2842fe9b4d3f2335d149
Patrice Chalin <pchalin@gmail.com>
2018-06-13 18:04:38 -0400

    chore: globally set DART_VM_OPTIONS=--preview-dart-2

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -22,8 +22,4 @@ travis_fold start before_install.npm_install
   (set -x; npm install --global firebase-tools gulp-cli superstatic --no-optional)
 travis_fold end before_install.npm_install
 
-travis_fold start before_install.linkcheck
-  (set -x; pub global activate linkcheck)
-travis_fold end before_install.linkcheck
-
 pub upgrade
