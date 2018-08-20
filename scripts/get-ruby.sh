8714f4eaa3f0653690ce90d49b89668b7777bbcb
Patrice Chalin <pchalin@gmail.com>
2018-08-20 14:52:16 -0400

    scripts: print ruby --version

--- scripts/get-ruby.sh
+++ scripts/get-ruby.sh
@@ -1,22 +1,30 @@
 # This bash file is meant to be source'd, not executed.
 
-SITE_WEBDEV_RUBY_VERS=2.4.3
+DART_SITE_RUBY_VERS=ruby-2.4.3
 
 if ! rvm version | grep -q '(latest)'; then
   rvm get stable
 fi
 
 echo "RVM current: $(rvm current)"
-if rvm current | grep -Fq "ruby-$SITE_WEBDEV_RUBY_VERS"; then
+if rvm current | grep -Fq $DART_SITE_RUBY_VERS; then
   echo "Using $(rvm current) (rvm $(rvm version | awk '{print $2}'))"
-elif rvm list | grep -Fq "ruby-$SITE_WEBDEV_RUBY_VERS"; then
-  rvm use $SITE_WEBDEV_RUBY_VERS
+elif rvm list | grep -Fq $DART_SITE_RUBY_VERS; then
+  if [[ -n "$TRAVIS" ]]; then DEFAULT="--default"; fi
+  local cmd="rvm $DEFAULT use $DART_SITE_RUBY_VERS"
+  echo "Running: $cmd"
+  $cmd
 elif [[ -z "$1" || "$1" == "--install" ]]; then
-  rvm install $SITE_WEBDEV_RUBY_VERS
+  local cmd="rvm install $DART_SITE_RUBY_VERS"
+  echo "Running: $cmd"
+  $cmd
 else
-  echo "ERROR: ruby $SITE_WEBDEV_RUBY_VERS isn't locally available and --install option was not specified."
+  echo "ERROR: ruby '$DART_SITE_RUBY_VERS' isn't locally available and --install option was not specified."
   return 1
 fi
+
+echo "Ruby --version: $(ruby --version)"
+rvm list
 
 if [[ ! -e ~/.rvmrc ]] || ! grep -q rvm_silence_path_mismatch_check_flag ~/.rvmrc;  then
     echo rvm_silence_path_mismatch_check_flag=1 >> ~/.rvmrc
