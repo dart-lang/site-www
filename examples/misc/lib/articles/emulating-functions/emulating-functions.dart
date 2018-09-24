9036a2d7fd12bce557f05e72f794d53ba4983018
Kathy Walrath <kathyw@google.com>
2018-09-24 16:00:02 -0700

    Update article & code

--- examples/misc/lib/articles/emulating-functions/emulating-functions.dart
+++ examples/misc/lib/articles/emulating-functions/emulating-functions.dart
@@ -4,16 +4,49 @@ class WannabeFunction {
 }
 // #enddocregion call-method
 
+abstract class Fooer {
+  // Define foo, so we can call NsmTest().foo().
+  foo();
+}
+
+class NsmTest extends Fooer {
+  baz() => print('Called baz!');
+
+  // #docregion no-such-method
+  dynamic noSuchMethod(Invocation invocation) {
+    return invocation.memberName == #foo
+        ? Function.apply(
+            baz, invocation.positionalArguments, invocation.namedArguments)
+        : super.noSuchMethod(invocation);
+  }
+  // #enddocregion no-such-method
+}
+
+typedef BinaryOperation<T> = T Function(T first, T second);
+BinaryOperation<int> add;
+
 void main() {
   // #docregion call-object-as-function
   var wf = WannabeFunction();
   assert(wf(3, 4) == 7);
   // #enddocregion call-object-as-function
 
+  // Confirm that WannabeFunction instances are assignable to
+  // BinaryOperations<int>.
+  add = wf;
+  assert(add(4, 5) == 9);
+
   // We describe the signature of apply() in the article. These lines check to
   // make sure that that signature hasn't changed.
-  var f = (String arg1, String arg2, {String arg3 = '!'}) {};
+  var f = (String arg1, String arg2, {String arg3 = '!'}) {
+    print('apply worked');
+  };
   var namedArgs = Map<Symbol, String>();
   namedArgs[const Symbol('arg3')] = '!!';
   Function.apply(f, ['hello', 'world'], namedArgs);
+
+  // Confirm that the unimplemented method foo() can be called (thanks to
+  // noSuchMethod().
+  var test = NsmTest();
+  test.foo();
 }
