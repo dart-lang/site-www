b770032700b795ac793bc180663b47015fab9e69
Michael Thomsen <mit@google.com>
2019-07-03 20:32:58 +0200

    Update .travis.yml to run linker checker on all builds (failures allowed) (#1719)

---
+++ tool/build-and-check-links.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+readonly rootDir="$(cd "$(dirname "$0")/.." && pwd)"
+
+# Build site
+bundle exec jekyll build
+
+# Check links
+$rootDir/tool/shared/check-links.sh
