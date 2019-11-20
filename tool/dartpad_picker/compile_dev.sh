a08af994a614e7b064b7b7217de62587cbaea707
Patrice Chalin <pchalin@gmail.com>
2019-11-20 16:48:53 -0500

    Remove dartpad_picker_main dev help files

--- tool/dartpad_picker/compile_dev.sh
+++ tool/dartpad_picker/compile_dev.sh
@@ -2,10 +2,12 @@
 
 : ${TMP:=tmp}
 OUT_DIR="$TMP/dartpad_picker"
+DEST_DIR=../../src/assets/dash/js
 
 set -x
 
 pub get
+rm $DEST_DIR/dartpad_picker*.*
 pub run build_runner build --output $OUT_DIR --define "build_web_compilers|entrypoint=compiler=dart2js"
-cp $OUT_DIR/web/* ../../src/assets/dash/js
+cp $OUT_DIR/web/*  $DEST_DIR
 rm -rf $OUT_DIR
