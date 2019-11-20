15b518b959dfaafda9fca6929720e06aed25f62b
Patrice Chalin <pchalin@gmail.com>
2019-11-20 12:09:39 -0500

    Dartpad_picker/compile scripts: don't wipe out entire tmp dir

--- tool/dartpad_picker/compile_dev.sh
+++ tool/dartpad_picker/compile_dev.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 : ${TMP:=tmp}
+OUT_DIR="$TMP/dartpad_picker"
 
 set -x
 
 pub get
-pub run build_runner build --output $TMP --define "build_web_compilers|entrypoint=compiler=dart2js"
-cp $TMP/web/* ../../src/assets/dash/js
-rm -rf $TMP
+pub run build_runner build --output $OUT_DIR --define "build_web_compilers|entrypoint=compiler=dart2js"
+cp $OUT_DIR/web/* ../../src/assets/dash/js
+rm -rf $OUT_DIR
