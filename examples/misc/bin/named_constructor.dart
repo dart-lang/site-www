c252804f084fd440360fca1f824005e8a107101a
Jaideep Prasad <jddeep003@gmail.com>
2020-03-04 17:05:27 +0530

    Create named_constructor.dart

---
+++ examples/misc/bin/named_constructor.dart
@@ -0,0 +1,16 @@
+class Point {
+  num x, y;
+
+  Point(this.x, this.y);
+
+  Point.origin() {
+    x = 0;
+    y = 0;
+  }
+}
+
+void main() {
+  final myPoint = Point.origin();
+  print(myPoint.x);
+  print(myPoint.y);
+}
