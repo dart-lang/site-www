db4cdebdf217caba80b48f5778f27b0c6a8fdef0
Jaideep Prasad <jddeep003@gmail.com>
2020-03-04 17:02:56 +0530

    Create this_constructor.dart

---
+++ examples/misc/bin/this_constructor.dart
@@ -0,0 +1,13 @@
+class MyColor {
+  int red;
+  int green;
+  int blue;
+
+  MyColor(this.red, this.green, this.blue);
+}
+
+final color = MyColor(80, 80, 128);
+
+void main() {
+  print(color.blue.toString());
+}
