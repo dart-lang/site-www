3399bc9e0b44d4734c9fffd40ba0f1c6f1ab3a4a
Jaideep Prasad <jddeep003@gmail.com>
2020-03-04 17:07:16 +0530

    Create factory_constructors.dart

---
+++ examples/misc/bin/factory_constructors.dart
@@ -0,0 +1,19 @@
+class Square extends Shape {}
+
+class Circle extends Shape {}
+
+class Shape {
+  Shape();
+
+  factory Shape.fromTypeName(String typeName) {
+    if (typeName == 'square') return Square();
+    if (typeName == 'circle') return Circle();
+
+    print('I don\'t recognize $typeName');
+    return null;
+  }
+}
+
+void main() {
+  final shape = Shape();
+}
