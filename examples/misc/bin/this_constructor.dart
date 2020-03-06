d7533cdb41e4e9c486fc5fb7cf5112907abce95d
Jaideep Prasad <jddeep003@gmail.com>
2020-03-06 08:17:59 +0530

    Update this_constructor.dart

--- examples/misc/bin/this_constructor.dart
+++ examples/misc/bin/this_constructor.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: sort_constructors_first, type_annotate_public_apis
+
+// #docregion this-constructor
 class MyColor {
   int red;
   int green;
@@ -8,6 +10,7 @@ class MyColor {
 }
 
 final color = MyColor(80, 80, 128);
+// #enddocregion this-constructor
 
 void main() {
   print(color.blue.toString());
