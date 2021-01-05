02985d165b719aa5892697025ef44dde3955d7a1
Tim Sneath <timsneath@google.com>
2021-01-05 15:07:56 -0800

    Amend comment per kwalrath review

--- examples/misc/lib/language_tour/variables.dart
+++ examples/misc/lib/language_tour/variables.dart
@@ -57,7 +57,7 @@ void miscDeclAnalyzedButNotTested() {
 
   {
     // #docregion const-dart-25
-    // Valid compile-time constants as of Dart 2.5.
+    // Some examples of valid compile-time constants
     const Object i = 3; // Where i is a const Object with an int value...
     const list = [i as int]; // Use a typecast.
     const map = {if (i is int) i: "int"}; // Use is and collection if.
