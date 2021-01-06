5c6f2db9905c664312812762c4057a77a6ee30e9
Tim Sneath <timsneath@google.com>
2021-01-05 16:49:25 -0800

    Update language-tour.md (#2815)
    Remove references to long-dead language versions. At this point, there's no need to document features added in 2.3 or call out that async keywords aren't supported in Dart 1.x :)

--- examples/misc/lib/language_tour/variables.dart
+++ examples/misc/lib/language_tour/variables.dart
@@ -57,7 +57,6 @@ void miscDeclAnalyzedButNotTested() {
 
   {
     // #docregion const-dart-25
-    // Valid compile-time constants as of Dart 2.5.
     const Object i = 3; // Where i is a const Object with an int value...
     const list = [i as int]; // Use a typecast.
     const map = {if (i is int) i: "int"}; // Use is and collection if.
