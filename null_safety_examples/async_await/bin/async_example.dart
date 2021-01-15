f32c17b72e6a9332f842561cb6752cb16bf4b774
Miguel Beltran <m@beltran.work>
2021-01-15 17:52:07 +0100

    Migrate Async-Await Codelab to Null Safety (#2857)
    * Copy of original async-await examples and codelab
    
    * migrated async_await example to null safety
    
    * Codelab migration to null safety
    
    - removed iframe dart-pad integration
    - code excerpts link to null_safety_examples/async_await
    - enabled null safety on dartpad examples
    - migrated practice_using dartpad exercise
    - migrate practice_error to null safety
    - add empty changeUsername to avoid 'undefined name' error
    - migrate putting_it_all_together to null safety
    
    * add missing test yaml files

---
+++ null_safety_examples/async_await/bin/async_example.dart
@@ -0,0 +1,25 @@
+Future<void> printOrderMessage() async {
+  print('Awaiting user order...');
+  // #docregion swap-stmts
+  var order = await fetchUserOrder();
+  // print('Awaiting user order...');
+  // #enddocregion swap-stmts
+  print('Your order is: $order');
+}
+
+Future<String> fetchUserOrder() {
+  // Imagine that this function is more complex and slow.
+  return Future.delayed(Duration(seconds: 4), () => 'Large Latte');
+}
+
+Future<void> main() async {
+  countSeconds(4);
+  await printOrderMessage();
+}
+
+// You can ignore this function - it's here to visualize delay time in this example.
+void countSeconds(int s) {
+  for (var i = 1; i <= s; i++) {
+    Future.delayed(Duration(seconds: i), () => print(i));
+  }
+}
