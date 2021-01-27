81a93ac6fc28a77ed2a5442861fc11a11262ab5c
Miguel Beltran <m@beltran.work>
2021-01-27 09:41:41 +0100

    copied language_tour code into null_safety_examples

---
+++ null_safety_examples/misc/lib/language_tour/classes/point_redirecting.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: sort_constructors_first
+class Point {
+  double x, y;
+
+  // The main constructor for this class.
+  Point(this.x, this.y);
+
+  // Delegates to the main constructor.
+  Point.alongXAxis(double x) : this(x, 0);
+}
