b977c3e6e99308286ca9b7fb56f2d29ac03c7d90
Kathy Walrath <kathyw@google.com>
2021-03-03 16:25:01 -0800

    Remove examples/misc/*/library_tour

--- examples/misc/lib/library_tour/core/exception.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: sort_constructors_first
-class FooException implements Exception {
-  final String msg;
-
-  const FooException([this.msg]);
-
-  @override
-  String toString() => msg ?? 'FooException';
-}
