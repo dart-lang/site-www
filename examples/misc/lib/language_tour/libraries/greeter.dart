937debbc7a9856583538166fd8adcecb7a989cec
Kathy Walrath <kathyw@google.com>
2021-03-08 15:12:15 -0800

    Move migrated pages on top of old pages (#3039)

--- examples/misc/lib/language_tour/libraries/greeter.dart
+++
@@ -1,9 +0,0 @@
-// #docregion import
-import 'hello.dart' deferred as hello;
-// #enddocregion import
-
-// #docregion loadLibrary
-Future greet() async {
-  await hello.loadLibrary();
-  hello.printGreeting();
-}
