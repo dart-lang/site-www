15ed16b69bf29be79291ec42f405d1d5c6efafcc
Miguel Beltran <m@beltran.work>
2021-03-18 16:25:19 +0100

    deleted old non-null code

--- examples/extension_methods/README.md
+++
@@ -1,2 +0,0 @@
-This package isn't an app. It's a collection of code excerpts and associated
-tests used in the docs.
--- examples/extension_methods/lib/string_extensions/string_apis_3.dart
+++
@@ -1,9 +0,0 @@
-extension NumberParsing on String {
-  int parseInt() {
-    return int.parse(this, radix: 16);
-  }
-
-  num parseNum() {
-    return num.parse(this);
-  }
-}
