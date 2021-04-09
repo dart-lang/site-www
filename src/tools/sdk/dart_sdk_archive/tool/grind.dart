edf4253f916f1496ff88d946950996ce19c381b2
Parker Lougheed <parlough@gmail.com>
2021-04-08 19:56:21 -0500

    Update the archive page supporting Dart code and packages

--- src/tools/sdk/dart_sdk_archive/tool/grind.dart
+++ src/tools/sdk/dart_sdk_archive/tool/grind.dart
@@ -5,11 +5,11 @@ import 'package:dart_sdk_archive/src/generator.dart';
 import 'package:grinder/grinder.dart';
 import 'package:dart_style/dart_style.dart';
 
-main(args) => grind(args);
+void main(args) => grind(args);
 
 @DefaultTask('Build SVN Revision JSON')
-buildJson() async {
-  print("fetching SVN versions...");
+void buildJson() async {
+  print('fetching SVN versions...');
   var filePath = 'lib/src/svn_versions.dart';
   var generator = SvnVersionGenerator();
   var svnVersions = await generator.svnVersions;
