7b6bc39f8d09b561e872ef9d642755e4f9843569
Parker Lougheed <parlough@gmail.com>
2021-04-27 13:10:42 -0500

    Add check for linter rules to diagnostic update check

--- tool/diagnostic_update_check.dart
+++ tool/diagnostic_update_check.dart
@@ -1,20 +1,21 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:crypto/crypto.dart' as crypto;
 import 'package:http/http.dart' as http;
 
 void main() async {
-  print('Verifying that the diagnostic messages file is up to date...');
+  print(
+      'Verifying that the diagnostic messages and linter rules files are up to date...');
 
-  // Uncomment once linter rules are added to website
-  // if (!await checkAsset(
-  //     'https://raw.githubusercontent.com/dart-lang/linter/gh-pages/lints/machine/rules.json',
-  //     '../src/_data/linter_rules.json')) {
-  //   print('The linter rules file is outdated.');
-  //   exit(1);
-  // }
+  // Check linter's machine generated rules file
+  if (!await checkAsset(
+      'https://raw.githubusercontent.com/dart-lang/linter/gh-pages/lints/machine/rules.json',
+      '../src/_data/linter_rules.json')) {
+    print('The linter rules file is outdated.');
+    exit(1);
+  }
 
+  // Check analyzer's diagnostic messages file
   if (!await checkAsset(
       'https://raw.githubusercontent.com/dart-lang/sdk/master/pkg/analyzer/tool/diagnostics/diagnostics.md',
       '../src/tools/diagnostic-messages.md')) {
@@ -22,9 +23,11 @@ void main() async {
     exit(1);
   }
 
-  print('The diagnostic messages file is up to date.');
+  print('The diagnostic messages and linter rules files are up to date.');
 }
 
+/// Check if the asset as the specified remote [url] and the local [path]
+/// share the same contents.
 Future<bool> checkAsset(final String url, final String path) async {
   final remote = await http.get(Uri.parse(url));
   if (remote.statusCode == 200) {
