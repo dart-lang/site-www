946ba1b4c5ba6d12f0022604fc2165482fdf4d4e
Parker Lougheed <parlough@gmail.com>
2021-07-28 13:55:26 -0500

    Fix skipWhile explanation and update surrounding snippets (#3420)
    * Fix skipWhile explanation and update surrounding snippets
    
    Fixes #2419
    
    * Add new line at EOF
    
    * Switch to const
    
    * Tweak wording slightly
    
    Co-authored-by: Kathy Walrath <kathyw@google.com>

---
+++ examples/iterables/bin/take_while.dart
@@ -0,0 +1,9 @@
+void main() {
+  const numbers = [1, 3, -2, 0, 4, 5];
+
+  var numbersUntilZero = numbers.takeWhile((number) => number != 0);
+  print('Numbers until 0: $numbersUntilZero');
+
+  var numbersStartingAtZero = numbers.skipWhile((number) => number != 0);
+  print('Numbers starting at 0: $numbersStartingAtZero');
+}
