208884ef0fcd5a6cd492089a02ead290d5fd1438
Parker Lougheed <parlough@gmail.com>
2021-09-21 13:30:55 -0500

    Patch dev build analysis failure (#3590)

--- examples/misc/lib/language_tour/typedefs/misc.dart
+++ examples/misc/lib/language_tour/typedefs/misc.dart
@@ -15,5 +15,6 @@ typedef Compare<T> = int Function(T a, T b);
 int sort(int a, int b) => a - b;
 
 void main() {
+  // ignore_for_file: stable, beta, dev, unnecessary_type_check
   assert(sort is Compare<int>); // True!
 }
