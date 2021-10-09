17c0ea4b3491b869c6e3522b8449bf84606e2a24
Andrew Lavrentev <andrew.lavrentev@gmail.com>
2021-10-09 08:59:57 +0300

    $(document.body)

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -28,9 +28,9 @@ $(window).scroll(function () {
   fixNav();
   var currentScreenPosition = $(document).scrollTop();
   if (currentScreenPosition > 50) {
-    $('document.body').addClass('fixed_nav');
+    $(document.body).addClass('fixed_nav');
   } else {
-    $('document.body').removeClass('fixed_nav');
+    $(document.body).removeClass('fixed_nav');
   }
 });
 
@@ -41,7 +41,7 @@ function adjustToc() {
     $('html, body').animate({ scrollTop: 0 }, 'fast');
   })
 
-  $('document.body').scrollspy({ offset: 100, target: '#site-toc--side' });
+  $(document.body).scrollspy({ offset: 100, target: '#site-toc--side' });
 }
 
 $(function () {
@@ -106,7 +106,7 @@ $(function () {
   // Frontpage popovers inside the #code-sample should scroll with the enclosing <pre>.
   setPopovers($('body.homepage #code-sample'), 'pre');
   // All other popovers should scroll with the page.
-  setPopovers($('document.body'), 'body');
+  setPopovers($(document.body), 'body');
 
 
   // open - close mobile navigation
@@ -118,9 +118,9 @@ $(function () {
   var topLevelMenuTogglers = ['#page-header', '.banner', '#page-content', '#page-footer'];
   for (var i = 0; i < topLevelMenuTogglers.length; i++) {
     $(topLevelMenuTogglers[i]).on('click', function (e) {
-      if ($('document.body').hasClass('open_menu')) {
+      if ($(document.body).hasClass('open_menu')) {
         e.preventDefault();
-        $('document.body').removeClass("open_menu");
+        $(document.body).removeClass("open_menu");
       }
     });
   }
