a256472f7cd316e24b0d140bc70e42fac593eb02
Parker Lougheed <parlough@gmail.com>
2022-02-09 16:33:57 -0600

    Enable failing build when at least one test fails

--- tool/test.sh
+++ tool/test.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 # Run all tests for validating code examples
 
-tool/check-formatting.sh
-tool/refresh-code-excerpts.sh
-tool/analyze-and-test-examples.sh
+set -u
+
+EXIT_STATUS=0
+
+tool/check-formatting.sh || EXIT_STATUS=1
+tool/refresh-code-excerpts.sh || EXIT_STATUS=1
+tool/analyze-and-test-examples.sh || EXIT_STATUS=1
 
 printf "\n-- Done --\n\n"
+
+if [[ $EXIT_STATUS == 0 ]]; then
+  printf "\n$(blue "All tests and checks have passed!")\n\n"
+else
+  printf "\n$(red "
+  Some tests and/or checks have failed.
+  Look at the full output from this script for details.
+  ")\n"
+fi
+
+exit $EXIT_STATUS
