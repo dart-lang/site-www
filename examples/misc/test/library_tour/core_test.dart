46df55bf69603f9c345483d0c3616d0506cfb432
Parker Lougheed <parlough@gmail.com>
2022-10-28 12:29:40 -0500

    Document URI http and https factory constructors (#4325)

--- examples/misc/test/library_tour/core_test.dart
+++ examples/misc/test/library_tour/core_test.dart
@@ -544,9 +544,20 @@ void main() {
           scheme: 'https',
           host: 'example.org',
           path: '/foo/bar',
-          fragment: 'frag');
-      assert(uri.toString() == 'https://example.org/foo/bar#frag');
+          fragment: 'frag',
+          queryParameters: {'lang': 'dart'});
+      assert(uri.toString() == 'https://example.org/foo/bar?lang=dart#frag');
       // #enddocregion Uri
+    });
+
+    test('http constructors', () {
+      // #docregion Uri-http
+      var httpUri = Uri.http('example.org', '/foo/bar', {'lang': 'dart'});
+      var httpsUri = Uri.https('example.org', '/foo/bar', {'lang': 'dart'});
+
+      assert(httpUri.toString() == 'http://example.org/foo/bar?lang=dart');
+      assert(httpsUri.toString() == 'https://example.org/foo/bar?lang=dart');
+      // #enddocregion Uri-http
     });
   });
 
