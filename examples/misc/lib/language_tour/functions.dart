d843bcc43d987080fe49bb25ee9a45e00dff4eac
Parker Lougheed <parlough@gmail.com>
2022-11-12 16:18:41 -0600

    Remove now unused function

--- examples/misc/lib/language_tour/functions.dart
+++ examples/misc/lib/language_tour/functions.dart
@@ -56,21 +56,6 @@ void miscDeclAnalyzedButNotTested() {
   }
 
   {
-    // #docregion list-map-default-function-param
-    void doStuff(
-        {List<int> list = const [1, 2, 3],
-        Map<String, String> gifts = const {
-          'first': 'paper',
-          'second': 'cotton',
-          'third': 'leather'
-        }}) {
-      print('list:  $list');
-      print('gifts: $gifts');
-    }
-    // #enddocregion list-map-default-function-param
-  }
-
-  {
     // #docregion function-as-param
     void printElement(int element) {
       print(element);
