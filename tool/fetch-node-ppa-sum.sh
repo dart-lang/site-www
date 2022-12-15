f75ea1399a0acb96c76735edd598e46611c7db9f
Parker Lougheed <parlough@gmail.com>
2022-12-14 22:12:36 -0600

    Enabling checking sums not in dev container

--- tool/fetch-node-ppa-sum.sh
+++ tool/fetch-node-ppa-sum.sh
@@ -4,5 +4,8 @@ set -eu -o pipefail
 
 NODE_PPA="node_ppa.sh"
 curl -fsSL https://deb.nodesource.com/setup_lts.x -o "$NODE_PPA"
-echo "NODE_SHA256="$(shasum -a 256 $NODE_PPA)"; \\"
+_checksum=$(shasum -a 256 $NODE_PPA)
+read -a _fname_arr <<< "${_checksum}" # Read in string output as array
+_checkonly="${_fname_arr%:*}" # Remove filename portion of checksum output
+echo "NODE_SHA256="$_checkonly"; \\"
 rm $NODE_PPA
