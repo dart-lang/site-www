187be61160e137c43adbab8996fb0ce06489df31
Marya <111139605+MaryaBelanger@users.noreply.github.com>
2022-12-20 14:00:47 -0800

    [2.19] Concurrency updates for isolate.run() (#4357)
    Updates 'Concurrency in Dart' page for `Isolate.run`.
    
    Closes #3904
    
    Co-authored-by: Parker Lougheed <parlough@gmail.com>

--- examples/concurrency/test/concurrency_test.dart
+++ examples/concurrency/test/concurrency_test.dart
@@ -4,9 +4,11 @@ import 'package:concurrency_examples/async_number_of_keys.dart'
     as async_number_of_keys;
 import 'package:concurrency_examples/simple_worker_isolate.dart'
     as simple_worker_isolate;
+import 'package:concurrency_examples/simple_isolate_closure.dart'
+    as simple_isolate_closure;
 import 'package:test/test.dart';
 
-void main() {
+Future<void> main() async {
   test('sync number of keys counts correctly', () {
     expect(() {
       sync_number_of_keys.main();
@@ -23,6 +25,13 @@ void main() {
   test('simple worker isolate counts number of keys correctly', () {
     expect(() async {
       simple_worker_isolate.main();
+      await Future.delayed(Duration(milliseconds: 500));
+    }, prints('Number of JSON keys: 3\n'));
+  });
+
+  test('simple closure isolate counts number of keys correctly', () {
+    expect(() async {
+      simple_isolate_closure.main();
       await Future.delayed(Duration(milliseconds: 500));
     }, prints('Number of JSON keys: 3\n'));
   });
