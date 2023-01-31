d7c49e7da3ba81fb2e10e36f2f3276168fba0221
Parker Lougheed <parlough@gmail.com>
2023-01-30 21:30:12 -0600

    Improve word breaking in TOC on underscores

---
+++ src/_plugins/underscore_breaker.rb
@@ -0,0 +1,7 @@
+module UnderscoreBreaker
+  def underscore_breaker(string_to_break)
+    return string_to_break.gsub('_', '_<wbr />')
+  end
+end
+
+Liquid::Template.register_filter(UnderscoreBreaker)
