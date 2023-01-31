dea092fd43fac38b4376ba3fff52f219a589e43a
Parker Lougheed <parlough@gmail.com>
2023-01-31 01:39:23 -0600

    Add some inline comments

--- src/_plugins/underscore_breaker.rb
+++ src/_plugins/underscore_breaker.rb
@@ -1,7 +1,13 @@
 module UnderscoreBreaker
+  # Adds `<wbr>` after underscores to allow the browser
+  # to more intelligently wrap identifiers and text split with underscores.
+  # If `in_anchor` is `true`, it will only replace the inner text content.
   def underscore_breaker(string_to_break, in_achor = false)
     return unless string_to_break.include? '_'
     if in_achor then
+      # If the replacement is to be done inside an anchor,
+      # we don't want to replace the href,
+      # just the inner text content.
       return string_to_break.gsub(/>([a-zA-Z_]*?)</) do |match|
         ">#{$1.gsub('_', '_<wbr>')}<"
       end
