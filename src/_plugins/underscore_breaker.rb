5b37a494e642ee9ef4aa57db70b116c4eec4b393
Parker Lougheed <parlough@gmail.com>
2023-01-30 22:51:23 -0600

    Don't affect the anchor href attribute too

--- src/_plugins/underscore_breaker.rb
+++ src/_plugins/underscore_breaker.rb
@@ -1,5 +1,12 @@
 module UnderscoreBreaker
-  def underscore_breaker(string_to_break)
+  def underscore_breaker(string_to_break, in_achor = false)
+    return unless string_to_break.include? '_'
+    if in_achor then
+      return string_to_break.gsub(/>([a-zA-Z_]*?)</) do |match|
+        ">#{$1.gsub('_', '_<wbr>')}<"
+      end
+    end
+
     return string_to_break.gsub('_', '_<wbr>')
   end
 end
