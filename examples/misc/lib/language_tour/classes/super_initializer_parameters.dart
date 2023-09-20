a83ae0fc14600b7e257b7016a34c6cd03cba751f
Parker Lougheed <parlough@gmail.com>
2023-09-20 12:29:40 -0500

    Fix duplicate close code excerpt updater error

--- examples/misc/lib/language_tour/classes/super_initializer_parameters.dart
+++ examples/misc/lib/language_tour/classes/super_initializer_parameters.dart
@@ -23,8 +23,7 @@ class Vector3d extends Vector2d {
   // #enddocregion named
   // #docregion positional
   final double z;
-
-  // #enddocregion named
+  
   // Forward the x and y parameters to the default super constructor like:
   // Vector3d(final double x, final double y, this.z) : super(x, y);
   Vector3d(super.x, super.y, this.z);
