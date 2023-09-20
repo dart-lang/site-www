e73f0028e13927708eb160c682652225e221ec15
Parker Lougheed <parlough@gmail.com>
2023-09-20 16:57:58 -0500

    Fix duplicate end docregion code excerpt updater error (#5210)

--- examples/misc/lib/language_tour/classes/super_initializer_parameters.dart
+++ examples/misc/lib/language_tour/classes/super_initializer_parameters.dart
@@ -24,7 +24,6 @@ class Vector3d extends Vector2d {
   // #docregion positional
   final double z;
 
-  // #enddocregion named
   // Forward the x and y parameters to the default super constructor like:
   // Vector3d(final double x, final double y, this.z) : super(x, y);
   Vector3d(super.x, super.y, this.z);
