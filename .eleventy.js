0be5cb0fae4f9ff2b3067ec5cc03a673b933bc0d
Parker Lougheed <parlough@gmail.com>
2023-11-06 21:06:50 -0600

    Expand HTML minification

--- .eleventy.js
+++ .eleventy.js
@@ -73,6 +73,7 @@ module.exports = function (eleventyConfig) {
           useShortDoctype: true,
           removeComments: true,
           collapseWhitespace: true,
+          minifyJS: true,
         });
       }
 
