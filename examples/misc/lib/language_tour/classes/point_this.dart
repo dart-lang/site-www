40ec9bf6535ec290f365a8cf2e44f6bb8546ddc1
Eric Windmill <ewindmill@google.com>
2024-01-03 09:40:46 -0500

    ignore example error in CI

--- examples/misc/lib/language_tour/classes/point_this.dart
+++ examples/misc/lib/language_tour/classes/point_this.dart
@@ -1,9 +1,10 @@
-// ignore_for_file: invalid_reference_to_this, unnecessary_this
+// ignore_for_file: unnecessary_this
 // #docregion this-late
 double initialX = 1.5;
 
 class Point {
   double? x = initialX; // OK, can access declarations that do not depend on `this`.
+  // ignore: stable, beta, dev, invalid_reference_to_this, invalid_assignment
   double? y = this.x; // ERROR, can't access `this` in non-`late` initializer.
   late double? z = this.x; // OK, can access `this` in `late` initializer.
 
