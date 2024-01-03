46ff8d59f5ec44284ea0fd6d130fdc99770371b1
Eric Windmill <ewindmill@google.com>
2024-01-03 09:52:12 -0500

    refresh snippets

--- examples/misc/lib/language_tour/classes/point_this.dart
+++ examples/misc/lib/language_tour/classes/point_this.dart
@@ -1,13 +1,14 @@
-// ignore_for_file: unnecessary_this
+// ignore_for_file: stable, beta, dev, invalid_reference_to_this, unnecessary_this
 // #docregion this-late
 double initialX = 1.5;
 
 class Point {
-  double? x = initialX; // OK, can access declarations that do not depend on `this`.
-  // ignore: stable, beta, dev, invalid_reference_to_this, invalid_assignment
+  double? x =
+      initialX; // OK, can access declarations that do not depend on `this`.
   double? y = this.x; // ERROR, can't access `this` in non-`late` initializer.
   late double? z = this.x; // OK, can access `this` in `late` initializer.
 
-  Point(this.x, this.y); // OK, `this.fieldName` is a parameter declaration, not an expression.
+  Point(this.x,
+      this.y); // OK, `this.fieldName` is a parameter declaration, not an expression.
 }
 // #enddocregion this-late
