a28b0d06bc5c50a8e50d33fc42866162ebe02ac8
Eric Windmill <ewindmill@google.com>
2024-01-03 09:55:09 -0500

    refresh snippets

--- examples/misc/lib/language_tour/classes/point_this.dart
+++ examples/misc/lib/language_tour/classes/point_this.dart
@@ -4,7 +4,6 @@ double initialX = 1.5;
 
 class Point {
   double? x = initialX; // OK, can access declarations that do not depend on `this`.
-  // ignore: stable, beta, dev, invalid_reference_to_this
   double? y = this.x; // ERROR, can't access `this` in non-`late` initializer.
   late double? z = this.x; // OK, can access `this` in `late` initializer.
 
