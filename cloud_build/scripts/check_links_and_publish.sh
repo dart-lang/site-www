709e61269c3558718267f78c8135046276725069
Khanh Nguyen <khanhnwin@gmail.com>
2024-01-03 19:09:26 -0800

    remove check links

--- cloud_build/scripts/check_links_and_publish.sh
+++ cloud_build/scripts/check_links_and_publish.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# Check links are valid 
-export TOOL_DIR=tool
-tool/check-links.sh
-
 # Deploy to Firebase hosting
 firebase deploy --project=flutter-website-staging --only=hosting
 
