3b98c4a37d7937934978d9718529ce0b4572ba16
Abhishek Anand <lex.abhishek@gmail.com>
2024-06-24 15:40:59 +0530

    Updated doc region in class modifiers example

--- examples/language/lib/class_modifiers/ex1/b.dart
+++ examples/language/lib/class_modifiers/ex1/b.dart
@@ -6,15 +6,16 @@ import 'a.dart';
 // Error: Can't be constructed.
 Vehicle myVehicle = Vehicle();
 
+
 // Can be extended.
 class Car extends Vehicle {
   int passengers = 4;
-  // #enddocregion abstract-usages
+  
   @override
   void moveForward(int meters) {
     // ...
   }
-  // #docregion abstract-usages
+  
 }
 
 // Can be implemented.
