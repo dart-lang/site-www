8bb93d4dd6fee1df7bb458383dcd757e0d5431b4
Parker Lougheed <parlough@gmail.com>
2024-10-11 21:11:29 -0500

    Split repository into a few workspaces

--- examples/misc/test/language_tour/functions_test.dart
+++ examples/misc/test/language_tour/functions_test.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: unused_element, type_annotate_public_apis, prefer_function_declarations_over_variables
+// ignore_for_file: unused_element, type_annotate_public_apis, prefer_function_declarations_over_variables, avoid_function_literals_in_foreach_calls
 // ignore_for_file: always_declare_return_types
+
 import 'package:examples/language_tour/function_equality.dart'
     as function_equality;
 import 'package:test/test.dart';
