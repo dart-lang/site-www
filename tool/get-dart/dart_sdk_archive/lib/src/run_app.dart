f0d51c546d262aefd7fc7be5fdc9fb0ffcd16eb3
Kilian Schulte <git@schultek.dev>
2025-01-06 22:00:48 +0100

    Migrate archive table to Jaspr

--- tool/get-dart/dart_sdk_archive/lib/src/run_app.dart
+++ tool/get-dart/dart_sdk_archive/lib/src/run_app.dart
@@ -1,39 +1,20 @@
 import 'dart:async';
 
 import 'package:http/browser_client.dart';
+import 'package:jaspr/browser.dart' as jaspr;
 import 'package:sdk_builds/sdk_builds.dart';
-import 'package:web/web.dart';
 
+import 'components/archive_table.dart';
 import 'version_selector.dart';
 
 Future<void> runApp() async {
   final client = DartDownloads(client: BrowserClient());
 
-  final stableSelector = VersionSelector(
-    'stable',
-    client,
-    document.getElementById('stable') as HTMLTableElement,
-    document.getElementById('stable-versions') as HTMLSelectElement,
-    document.getElementById('stable-os') as HTMLSelectElement,
-  );
-
-  final betaSelector = VersionSelector(
-    'beta',
-    client,
-    document.getElementById('beta') as HTMLTableElement,
-    document.getElementById('beta-versions') as HTMLSelectElement,
-    document.getElementById('beta-os') as HTMLSelectElement,
-  );
-
-  final devSelector = VersionSelector(
-    'dev',
-    client,
-    document.getElementById('dev') as HTMLTableElement,
-    document.getElementById('dev-versions') as HTMLSelectElement,
-    document.getElementById('dev-os') as HTMLSelectElement,
-  );
-
-  unawaited(stableSelector.init());
-  unawaited(betaSelector.init());
-  unawaited(devSelector.init());
+  for (final channel in ['stable', 'beta', 'dev']) {
+    final selector = VersionSelector(channel: channel, client: client);
+    jaspr.runApp(
+      ArchivesTable(selector: selector),
+      attachTo: '.archive-table[data-channel="$channel"]',
+    );
+  }
 }
