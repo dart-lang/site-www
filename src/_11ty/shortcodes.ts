5b41f4ea46f9be00643c9e879d0dae96abe07dd0
Parker Lougheed <parlough@gmail.com>
2025-05-31 17:29:46 +0800

    Add and use tab shortcodes from flutter docs (#6628)
    Copies with `{% tabs %}` implementation from flutter/website with minor
    modifications to be compatible with the site. Then replaces existing
    tabs manually built with HTML with the new functionality.
    
    This change also results in improved screen reader and keyboard
    accessibility for the migrated tabs.

--- src/_11ty/shortcodes.ts
+++ src/_11ty/shortcodes.ts
@@ -1,9 +1,14 @@
 import {UserConfig} from '@11ty/eleventy';
+import {slugify} from './utils/slugify.js';
+import {fromHtml} from 'hast-util-from-html';
+import {selectAll} from 'hast-util-select';
+import {toHtml} from 'hast-util-to-html';
 import {escapeHtml} from 'markdown-it/lib/common/utils.mjs';
 
 export function registerShortcodes(eleventyConfig: UserConfig): void {
   _setupCards(eleventyConfig);
   _setupMedia(eleventyConfig);
+  _setupTabs(eleventyConfig);
 }
 
 function _setupCards(eleventyConfig: UserConfig): void {
@@ -54,5 +59,64 @@ function _setupMedia(eleventyConfig: UserConfig): void {
     wrapperMarkup += content;
     wrapperMarkup += '</div>';
     return wrapperMarkup;
+  });
+}
+
+function _setupTabs(eleventyConfig: UserConfig) {
+  // Counter shared between all tabs and wrappers to
+  // ensure each has a unique ID.
+  let currentTabWrapperId = 0;
+  let currentTabPaneId = 0;
+
+  eleventyConfig.addPairedShortcode('tabs', function (content: string, saveKey: string, wrapped: boolean = false) {
+    const tabWrapperId = currentTabWrapperId++;
+    let tabMarkup = `<div id="${tabWrapperId}" class="tabs-wrapper${wrapped ? " wrapped" : ""}" ${saveKey ? `data-tab-save-key="${slugify(saveKey)}"` : ''}><ul class="nav-tabs" role="tablist">`;
+
+    // Only select child tab panes that don't already have a parent wrapper.
+    const tabPanes = selectAll('.tab-pane[data-tab-wrapper-id="undefined"]', fromHtml(content));
+    if (tabPanes.length <= 1) {
+      throw new Error(`Tabs with save key of ${saveKey} needs more than one tab!`);
+    }
+
+    let setTabToActive = true;
+    for (const tabPane of tabPanes) {
+      // Keep track of the tab wrapper ID to avoid including
+      // a duplicate of this tab's contents in a parent wrapper.
+      tabPane.properties.dataTabWrapperId = tabWrapperId;
+
+      const tabId = tabPane.properties.dataTabId! as string;
+      const tabPanelId = `${tabId}-panel`;
+      const tabName = tabPane.properties.dataTabName! as string;
+      const tabIsActive = setTabToActive;
+
+      // Only set the first tab of a wrapper to active initially.
+      if (tabIsActive) {
+        tabPane.properties['className'] += ' active';
+        setTabToActive = false;
+      }
+
+      const tabSaveId = slugify(tabName);
+      tabMarkup += `<li class="nav-item">
+  <a class="nav-link ${tabIsActive ? "active" : ""}" tabindex="0" data-tab-save-id="${tabSaveId}" id="${tabId}" href="#${tabPanelId}" role="tab" aria-controls="${tabPanelId}" aria-selected="${tabIsActive ? "true" : "false"}">${tabName}</a>
+</li>`;
+    }
+
+    tabMarkup += '</ul><div class="tab-content">\n';
+    tabMarkup += toHtml(tabPanes);
+    tabMarkup += '\n</div></div>';
+
+    return tabMarkup;
+  });
+
+  eleventyConfig.addPairedShortcode('tab', function (content: string, tabName: string) {
+    const tabIdNumber = currentTabPaneId++;
+    const tabId = `${tabIdNumber}-tab`;
+    const tabPanelId = `${tabId}-panel`;
+    return `<div class="tab-pane" id="${tabPanelId}" role="tabpanel" aria-labelledby="${tabId}" data-tab-id="${tabId}" data-tab-name="${tabName}" data-tab-wrapper-id="undefined">
+
+${content}
+
+</div>
+`;
   });
 }
