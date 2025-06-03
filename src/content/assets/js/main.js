b585d731af87ce2ceb576a1db49d9ddda190b29d
Parker Lougheed <parlough@gmail.com>
2025-06-04 00:50:45 +0800

    Add initial feedback widget to trailing site content (#6641)
    Resolves https://github.com/dart-lang/site-www/issues/6637, using the
    same tag/event IDs as `flutter/website`.

--- src/content/assets/js/main.js
+++ src/content/assets/js/main.js
@@ -295,6 +295,28 @@ function setupExpandableCards() {
   });
 }
 
+function setupFeedback() {
+  const feedbackContainer =
+      document.getElementById('page-feedback');
+  if (!feedbackContainer) return;
+
+  const feedbackUpButton = feedbackContainer.querySelector('#feedback-up-button');
+  const feedbackDownButton = feedbackContainer.querySelector('#feedback-down-button');
+  if (!feedbackUpButton || !feedbackDownButton) return;
+
+  feedbackUpButton.addEventListener('click', (_) => {
+    window.dataLayer?.push({'event': 'inline_feedback', 'feedback_type': 'up'});
+
+    feedbackContainer.classList.add('feedback-up');
+  }, { once: true });
+
+  feedbackDownButton.addEventListener('click', (_) => {
+    window.dataLayer?.push({'event': 'inline_feedback', 'feedback_type': 'down'});
+
+    feedbackContainer.classList.add('feedback-down');
+  }, { once: true });
+}
+
 function _setupSite() {
   setupTheme();
   setupSidenav();
@@ -354,6 +376,7 @@ function _setupSite() {
 
   setupCopyButtons();
   setupExpandableCards();
+  setupFeedback();
 }
 
 // Run setup if DOM is loaded, otherwise do it after it has loaded.
