558626a28eb482393b9365bb34516ba044434799
Parker Lougheed <parlough@gmail.com>
2025-12-19 10:32:59 -0600

    Fix lints and make utils private

--- site/lib/src/components/tutorial/quiz.dart
+++ site/lib/src/components/tutorial/quiz.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:collection/collection.dart';
 import 'package:jaspr/jaspr.dart';
 import 'package:jaspr_content/jaspr_content.dart';
 import 'package:yaml/yaml.dart';
@@ -30,7 +29,7 @@ class Quiz extends CustomComponent {
       final data = loadYamlNode(content);
       assert(data is YamlList, 'Invalid Quiz content. Expected a YAML list.');
       final questions = (data as YamlList).nodes
-          .map((n) => parseQuestion(n as YamlMap))
+          .map((n) => _parseQuestion(n as YamlMap))
           .toList();
       assert(questions.isNotEmpty, 'Quiz must contain at least one question.');
       return InteractiveQuiz(title: title, questions: questions);
@@ -39,19 +38,15 @@ class Quiz extends CustomComponent {
   }
 }
 
-Question parseQuestion(YamlMap map) {
-  return Question(
-    parseMarkdownToHtml(map['question'] as String, inline: true),
-    (map['options'] as List<Object?>)
-        .map((e) => parseAnswer(e as YamlMap))
-        .toList(),
-  );
-}
+Question _parseQuestion(YamlMap map) => Question(
+  parseMarkdownToHtml(map['question'] as String, inline: true),
+  (map['options'] as List<Object?>)
+      .map((e) => _parseAnswer(e as YamlMap))
+      .toList(),
+);
 
-AnswerOption parseAnswer(YamlMap map) {
-  return AnswerOption(
-    parseMarkdownToHtml(map['text'] as String, inline: true),
-    map['correct'] as bool? ?? false,
-    parseMarkdownToHtml(map['explanation'] as String),
-  );
-}
+AnswerOption _parseAnswer(YamlMap map) => AnswerOption(
+  parseMarkdownToHtml(map['text'] as String, inline: true),
+  map['correct'] as bool? ?? false,
+  parseMarkdownToHtml(map['explanation'] as String),
+);
